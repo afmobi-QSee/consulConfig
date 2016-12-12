4a1d166b8fe0b2e41a9ba5ed5ae4d97143fe38c8
wenzhanbo <zhanbo.wen@afmobigroup.com>
2016-12-12 15:23:57 +0800

    print msg

--- service.go
+++ service.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"io/ioutil"
 	"bytes"
+	"fmt"
 )
 
 type Check struct {
@@ -40,7 +41,8 @@ func (this *ConsulConfig)Register() error{
 	service := &Service{Id:this.ServiceName + this.Ip + strconv.Itoa(this.Port), Name:this.ServiceName, Address:this.Ip, Port:this.Port, Tags: this.Tag, Check:[]Check{*check}}
 	jbody, _ := json.Marshal(service)
 	client := &http.Client{}
-	req, err := http.NewRequest("PUT", "http://" + this.Url + "/v1/agent/service/register", bytes.NewReader(jbody))
+	fmt.Print("PUT", "http://" + this.Url + "/v1/agent/service/register", string(jbody))
+	req, err := http.NewRequest("PUT ", "http://" + this.Url + "/v1/agent/service/register ", bytes.NewReader(jbody))
 	if err != nil{
 		return err
 	}
--- service_test.go
+++ service_test.go
@@ -3,7 +3,7 @@ package consulConfig
 import "testing"
 
 func Test_service(t *testing.T){
-	consulConfig := NewConsulConfig("172.17.40.21:8500", "testServiceName", "10.2.227.131", 6379, []string{"testTag"})
+	consulConfig := NewConsulConfig("127.0.0.1:8500", "testServiceName", "10.2.227.131", 6379, []string{"testTag"})
 	consulConfig.Register()
 	consulConfig.DeRegister()
 }
