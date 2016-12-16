1920b6365310a64ed24653448ab744385685eaca
wenzhanbo <zhanbo.wen@afmobigroup.com>
2016-12-16 16:25:39 +0800

    add getlocalip function

--- util.go
+++ util.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net"
 )
 
 func httpGet(url string) (string) {
@@ -21,4 +22,22 @@ func httpGet(url string) (string) {
 	}
 
 	return string(body)
+}
+
+func GetLocalIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return err.Error()
+	}
+	var localIp string
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				localIp = ipnet.IP.String()
+				break
+			}
+		}
+	}
+	return localIp
 }
--- util_test.go
+++ util_test.go
@@ -1,10 +1,18 @@
 package consulConfig
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func Test_httpGet(t *testing.T){
 	result := httpGet("http://www.github.com")
 	if result == ""{
 		t.Error("httpGet test failed")
 	}
+}
+
+func Test_GetLocalIp(t *testing.T){
+	ip := GetLocalIp();
+	fmt.Print(ip)
 }
